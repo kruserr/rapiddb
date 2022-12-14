9b546d450a3b2c51258c44c38654e50750b7e24a
kruserr <46799551+kruserr@users.noreply.github.com>
2022-12-14 15:07:53 +0000

    fix: refactor and run ci before tagging

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,59 +1,60 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail
+
+ci () {
+  cargo +nightly fmt --all
+  cargo clippy --all-targets --all-features -- -Dwarnings
+  cargo test
+}
+
+bump_version () {
+  # update the Cargo.toml version of the rapiddb workspaces
+  msg="# prepare_release.sh"
+  sed "s/^version = .* $msg$/version = \"${1#v}\" $msg/" -i rapiddb/Cargo.toml
+
+  cargo check
+}
+
+prepare_tag () {
+  # generate a changelog for the tag message
+  export GIT_CLIFF_TEMPLATE="\
+  {% for group, commits in commits | group_by(attribute=\"group\") %}
+  {{ group | upper_first }}\
+  {% for commit in commits %}
+    - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
+  {% endfor %}
+  {% endfor %}"
+  changelog=$(git-cliff --config git-cliff-detailed.toml --unreleased --strip all)
+  
+  git add -A && git commit -m "chore(release): prepare for $1"
+
+  git fetch --all --tags
+  git tag -a "$1" -m "# Release $1" -m "$changelog"
+  git tag -v "$1"
+}
 
 # takes the tag as an argument (e.g. v0.1.0)
-if [ -n "$1" ]; then
+if [ -n $1 ]; then
   if [ $1 == "init" ]; then
-    if [ -n "$2" ]; then
-      # update the Cargo.toml version of the rapiddb workspaces
-      msg="# managed by release.sh"
-      sed "s/^version = .* $msg$/version = \"${2#v}\" $msg/" -i rapiddb/Cargo.toml
+    if [ -n $2 ]; then
+      ci
 
-      cargo check
+      bump_version $2
       
       git-cliff --tag "$2" > CHANGELOG.md
 
-      # generate a changelog for the tag message
-      export GIT_CLIFF_TEMPLATE="\
-      {% for group, commits in commits | group_by(attribute=\"group\") %}
-      {{ group | upper_first }}\
-      {% for commit in commits %}
-        - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-      {% endfor %}
-      {% endfor %}"
-      changelog=$(git-cliff --config git-cliff-detailed.toml --unreleased --strip all)
-
-      git add -A && git commit -m "chore(release): prepare for $2"
-
-      git fetch --all --tags
-      git tag -a "$2" -m "# Release $2" -m "$changelog"
-	    git tag -v "$2"
+      prepare_tag $2
     else
       echo "warn: please provide a tag"
     fi
   else
-    # update the Cargo.toml version of the rapiddb workspaces
-    msg="# managed by release.sh"
-    sed "s/^version = .* $msg$/version = \"${1#v}\" $msg/" -i rapiddb/Cargo.toml
-
-    cargo check
+    ci
+    
+    bump_version $1
     
     git-cliff --unreleased --tag "$1" --prepend CHANGELOG.md
 
-    # generate a changelog for the tag message
-    export GIT_CLIFF_TEMPLATE="\
-    {% for group, commits in commits | group_by(attribute=\"group\") %}
-    {{ group | upper_first }}\
-    {% for commit in commits %}
-      - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-    {% endfor %}
-    {% endfor %}"
-    changelog=$(git-cliff --config git-cliff-detailed.toml --unreleased --strip all)
-    
-    git add -A && git commit -m "chore(release): prepare for $1"
-
-    git fetch --all --tags
-    git tag -a "$1" -m "# Release $1" -m "$changelog"
-    git tag -v "$1"
+    prepare_tag $1
   fi
 else
 	echo "warn: please provide a tag"
