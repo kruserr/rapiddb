783072755f6a3baaa8daea0f63c676e054eddcd0
kruserr <46799551+kruserr@users.noreply.github.com>
2023-11-25 15:36:38 +0000

    feat: #13: basic MMAVAsyncDatabase implementation

--- rapiddb/src/db/mmav/mod.rs
+++ rapiddb/src/db/mmav/mod.rs
@@ -1,7 +1,9 @@
 //! RapidDB Databases
 
 mod mmav;
-mod mmav_database;
 mod mmav_unit;
+mod mmav_database;
+mod mmav_async_database;
 
 pub use mmav_database::MMAVDatabase;
+pub use mmav_async_database::MMAVAsyncDatabase;
--- rapiddb/src/db/mod.rs
+++ rapiddb/src/db/mod.rs
@@ -2,8 +2,7 @@
 
 mod database_test_factory;
 mod mmav;
-// mod mmav_database;
-// mod mmav_unit;
 
 pub use database_test_factory::DatabaseTestFactory;
 pub use mmav::MMAVDatabase;
+pub use mmav::MMAVAsyncDatabase;
