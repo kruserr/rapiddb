9b2f3a5acd51f9bbb5bfe0216d5f20235d954a8e
kruserr <46799551+kruserr@users.noreply.github.com>
2023-11-30 19:03:01 +0000

    feat: #17: full serialization of whole hashmap each write

--- rapiddb-web/src/main.rs
+++ rapiddb-web/src/main.rs
@@ -1,6 +1,20 @@
-#[tokio::main]
-async fn main() {
-  let db = rapiddb::db::MMAVAsyncDatabase::new();
+// #[tokio::main]
+// async fn main() {
+//   let db = rapiddb::db::MMAVAsyncDatabase::new();
 
-  warp::serve(rapiddb_web::api::endpoints(db)).run(([0, 0, 0, 0], 3030)).await;
+//   warp::serve(rapiddb_web::api::endpoints(db)).run(([0, 0, 0, 0], 3030)).await;
+// }
+
+use rapiddb::traits::IDatabase;
+fn main() {
+  let mut db = rapiddb::db::MmapDatabase::new("test.db").unwrap();
+
+  let value = b"{\"key-2\": \"value-2\"}";
+  // db.post("test-1", value);
+  println!("{:?}", String::from_utf8(db.get_latest("test-0")));
+  println!("{:?}", String::from_utf8(db.get_latest("test-1")));
+
+  println!("{:?}", String::from_utf8(db.get("test-1", 0)));
+  println!("{:?}", String::from_utf8(db.get("test-1", 1)));
+  println!("{:?}", String::from_utf8(db.get("test-0", 0)));
 }
--- rapiddb/src/db/mmhm_db/mod.rs
+++ rapiddb/src/db/mmhm_db/mod.rs
@@ -1,42 +1,131 @@
+use bincode::{deserialize_from, serialize_into};
+use memmap2::MmapMut;
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::sync::Mutex;
-use memmap2::MmapMut;
-use serde_derive::{Serialize, Deserialize};
-use bincode::{serialize_into, deserialize_from};
+
+use crate::traits::IDatabase;
 
 #[derive(Serialize, Deserialize)]
 struct SensorData {
-    meta: Vec<u8>,
-    records: Vec<Vec<u8>>,
+  meta: Vec<u8>,
+  records: Vec<Vec<u8>>,
 }
 
 pub struct MmapDatabase {
-    data: Mutex<HashMap<String, SensorData>>,
-    mmap: MmapMut,
+  data: Mutex<HashMap<String, SensorData>>,
+  mmap: MmapMut,
 }
 
 impl MmapDatabase {
-    pub fn new(path: &str) -> std::io::Result<Self> {
-        let file = OpenOptions::new().read(true).write(true).create(true).open(path)?;
-        file.set_len(4096)?; // Set initial size of the file
-        let mmap = unsafe { MmapMut::map_mut(&file)? };
+  pub fn new(path: &str) -> std::io::Result<Self> {
+    let file =
+      OpenOptions::new().read(true).write(true).create(true).open(path)?;
+    file.set_len(4096)?; // Set initial size of the file
+    let mmap = unsafe { MmapMut::map_mut(&file)? };
 
-        let data: HashMap<String, SensorData> = match deserialize_from(&*mmap) {
-            Ok(data) => data,
-            Err(_) => HashMap::new(), // If deserialization fails, create a new HashMap
-        };
+    let data: HashMap<String, SensorData> = match deserialize_from(&*mmap) {
+      Ok(data) => data,
+      Err(_) => HashMap::new(), // If deserialization fails, create a new HashMap
+    };
 
-        Ok(Self {
-            data: Mutex::new(data),
-            mmap,
-        })
+    Ok(Self { data: Mutex::new(data), mmap })
+  }
+
+  fn save(&mut self) -> bincode::Result<()> {
+    let data = self.data.lock().unwrap();
+    serialize_into(&mut *self.mmap, &*data)
+  }
+}
+
+impl IDatabase for MmapDatabase {
+  fn contains(&self, id: &str) -> bool {
+    let data = self.data.lock().unwrap();
+    data.contains_key(id)
+  }
+
+  fn get(&mut self, id: &str, rec_id: usize) -> Vec<u8> {
+    let data = self.data.lock().unwrap();
+    data.get(id).unwrap().records[rec_id].clone()
+  }
+
+  fn post(&mut self, id: &str, value: &[u8]) {
+    {
+      let mut data = self.data.lock().unwrap();
+      data
+        .entry(id.to_string())
+        .or_insert_with(|| SensorData { meta: vec![], records: vec![] })
+        .records
+        .push(value.to_vec());
     }
+    self.save().unwrap();
+  }
 
-    fn save(&mut self) -> bincode::Result<()> {
-        let data = self.data.lock().unwrap();
-        serialize_into(&mut *self.mmap, &*data)
+  fn get_meta(&mut self, id: &str) -> Vec<u8> {
+    let data = self.data.lock().unwrap();
+    data.get(id).unwrap().meta.clone()
+  }
+
+  fn post_meta(&mut self, id: &str, value: Vec<u8>) {
+    {
+      let mut data = self.data.lock().unwrap();
+      data
+        .entry(id.to_string())
+        .or_insert_with(|| SensorData { meta: vec![], records: vec![] })
+        .meta = value;
     }
+    self.save().unwrap();
+  }
 
-    // ... implement other methods ...
+  fn get_aggregates(&self, id: &str) -> Vec<u8> {
+    // This method isn't typically associated with a HashMap.
+    // You might need to store additional metadata in the SensorData struct.
+    unimplemented!()
+  }
+
+  fn get_latest(&mut self, id: &str) -> Vec<u8> {
+    let data = self.data.lock().unwrap();
+    let sensor_data = data.get(id).unwrap();
+    sensor_data.records.last().unwrap().clone()
+  }
+
+  fn get_latest_with_limit(&mut self, id: &str, limit: usize) -> Vec<Vec<u8>> {
+    let data = self.data.lock().unwrap();
+    let sensor_data = data.get(id).unwrap();
+    sensor_data.records.iter().rev().take(limit).cloned().collect()
+  }
+
+  fn get_range(&mut self, id: &str, start: usize, end: usize) -> Vec<Vec<u8>> {
+    let data = self.data.lock().unwrap();
+    let sensor_data = data.get(id).unwrap();
+    sensor_data.records[start..end].to_vec()
+  }
+
+  fn get_all_meta(&mut self) -> HashMap<&str, Vec<u8>> {
+    unimplemented!()
+    // let data = self.data.lock().unwrap();
+    // data.iter().map(|(id, sensor_data)| (id.clone(), sensor_data.meta.clone())).collect()
+  }
+
+  fn get_all_aggregates(&self) -> std::collections::HashMap<&str, Vec<u8>> {
+    // This method isn't typically associated with a HashMap.
+    // You might need to store additional metadata in the SensorData struct.
+    unimplemented!()
+  }
+
+  fn get_all_latest(&mut self) -> HashMap<&str, Vec<u8>> {
+    unimplemented!()
+    // let data = self.data.lock().unwrap();
+    // data.iter().map(|(id, sensor_data)| (id.clone(), sensor_data.records.last().unwrap().clone())).collect()
+  }
+
+  fn get_all_latest_with_limit(
+    &mut self,
+    limit: usize,
+  ) -> HashMap<&str, Vec<Vec<u8>>> {
+    unimplemented!()
+    // let data = self.data.lock().unwrap();
+    // data.iter().map(|(id, sensor_data)| (id.clone(), sensor_data.records.iter().rev().take(limit).cloned().collect())).collect()
+  }
 }
