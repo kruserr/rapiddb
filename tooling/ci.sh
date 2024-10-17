c1596ca803e6be2ed7249b7c64922921d52eccc6
kruserr <46799551+kruserr@users.noreply.github.com>
2024-10-17 20:22:46 +0000

    ci: update ci tooling

--- tooling/ci.sh
+++ tooling/ci.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# Deps
+# cargo install --locked cargo-audit cargo-edit cargo-udeps cargo-geiger cargo-crev cargo-deny
+
 set -Eeuo pipefail
 
-cargo +nightly fmt --all
-cargo clippy --all-targets --all-features -- -Dwarnings
-cargo test
+ci () {
+
+  cargo audit
+  cargo upgrade --verbose
+  cargo update --verbose
+
+  cargo +nightly fmt --all
+  cargo clippy --all-targets --all-features -- -Dwarnings
+  cargo test
+
+  cargo +nightly udeps --all-targets
+}
+
+ci
